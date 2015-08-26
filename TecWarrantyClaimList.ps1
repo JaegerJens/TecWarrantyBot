3600fb274c2507a498a8739ad0933fbe231a3ea4
Jens Jaeger <jaegerj204@googlemail.com>
2015-08-26 11:16:14 +0200

    powershell script for web request of decider claim list

---
+++ TecWarrantyClaimList.ps1
@@ -0,0 +1,11 @@
+﻿$loginUrl="https://staging-tecwarranty-cld.tecalliance.net/Account/Login"
+$deciderListUrl="https://staging-tecwarranty-cld.tecalliance.net/Grid/ClaimOverviewDeciderData"
+$r=Invoke-WebRequest $loginUrl -SessionVariable sess
+$form=$r.Forms
+$loginForm=$form[1]
+$loginForm.Fields["UserName"]="skf_decider"
+$loginForm.Fields["Password"]="......"
+$r=Invoke-WebRequest -Uri $loginUrl -WebSession $sess -Method Post -Body $loginForm.Fields
+
+
+$r=Invoke-WebRequest -Uri $deciderListUrl -WebSession $sess -Method Post
